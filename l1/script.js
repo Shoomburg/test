feaef8ce61e1a850c4ba4e8a7a1c958b2306ed24
Alex <ekbbrooklyn@gmail.com>
2018-05-31 02:31:38 +0500

    Первый комит

---
+++ l1/script.js
@@ -0,0 +1,60 @@
+var first = 1;
+
+const pi = 3.14;
+
+{
+	let second = 2;
+}
+
+console.log(4/0);
+console.log('string' * 9);
+
+// console.log(n1);
+
+var und;
+console.log(und);
+
+obj = {
+	name: "John",
+	age: 25,
+	isMarried: false
+}
+
+console.log(obj.name);
+console.log(obj["name"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[0]);
+console.log(arr[2]);
+
+// alert("Hello");
+// confirm("Are you here?");
+// var answer = +prompt("Are you 18?", "18");
+
+// console.log( typeof(answer));
+// console.log( typeof(arr));
+
+// console.log('arr' + " - object");
+
+var icnr = 10;
+		decr = 10;
+
+// icnr++;
+// decr--;
+
+// console.log(icnr++);
+// console.log(decr--);
+
+console.log(10%4);
+
+console.log(icnr == 10);
+console.log('10' === 10);
+
+var isChecked = true;
+var isClose = true;
+
+console.log(isChecked && isClose);
+console.log(isChecked && !isClose);
+
+console.log(isChecked || !isClose);
+
