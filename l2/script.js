faf114190a0d0a59fdecf9faedf6d2d0c5c76b8e
Alex <ekbbrooklyn@gmail.com>
2018-06-01 17:27:41 +0500

    Урок 2

--- l2/script.js
+++ l2/script.js
@@ -1,3 +1,17 @@
-if(4*2 == 8*1) {
+if(4*2 == 7*1) {
 	console.log('OK')
+} else {
+	console.log('No')
 }
+
+let num = 50;
+
+if(num < 49) {
+	console.log('NO')
+} else if(num > 100) {
+		consolelog('NO')
+	} else {
+		console.log('OK!')
+	}
+
+(num == 50)? console.log('OK!') : console.log('NO');
