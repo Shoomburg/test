b7dd0dd65619788743679a683c63c46749f74334
Alex <ekbbrooklyn@gmail.com>
2018-06-02 01:49:22 +0500

    ДЗ урок 3

--- dznorm/script.js
+++ dznorm/script.js
@@ -1,26 +1,43 @@
-let money = prompt("Ваш бюджет?");
-let name = prompt("Название Вашего магазина?");
-let time = 19;
+let money,
+		name,
+		time,
+		price
+
+function  start() {
+	money = +prompt("Ваш бюджет?");
+
+	while (isNaN(money) || money == "" || money == null) {
+		alert("Вы ввели неправильное значение или вообще ничего не ввели, попробуйте еще раз");
+		money = prompt("Ваш бюджет?");
+	}
+
+	name = prompt("Название Вашего магазина?").toUpperCase();
+	time = 21;
+}
+// start();
 
 let mainList = {
 	budget: money,
 	shopName: name,
 	shopGoods: [],
 	employers: {},
-	open: false
+	open: false,
+	discount: true
 }
 
-for (let i = 0; i < 5; i++) {
-
-	let a = prompt("Какой тип товаров будем продавать?" + (i + 1));
-	if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
-		console.log('Все верно');
-		mainList.shopGoods[i] = a;
-	} else {
-		alert("Вы ввели неправильное значение или вообще ничего не ввели, попробуйте еще раз");
-		i = i - 1;
+function chooseGoods() {
+	for (let i = 0; i < 5; i++) {
+		let a = prompt("Введите товар № " + (i + 1));
+		if ((typeof(a)) === 'string' && (typeof(a)) != null && a != '' && a.length < 50 ) {
+			console.log('Все верно');
+			mainList.shopGoods[i] = a;
+		} else {
+			alert("Вы ввели неправильное значение или вообще ничего не ввели, попробуйте еще раз");
+			i = i - 1;
+		}
 	}
 }
+// chooseGoods();
 
 // let i = 0;
 // while (i < 5) {
@@ -51,16 +68,43 @@ for (let i = 0; i < 5; i++) {
 // }
 // while (i < 5);
 
-if (time < 0) {
-	console.log("Этого не может быть");
-} else if(time > 8 && time < 20) {
-	console.log('Время работать');
-} else if(time < 24) {
-	console.log('Слицком поздно');
-} else {
-	console.log('В сутках 24 часа!');
-};
+function workTime(time) {
+	if (time < 0) {
+		console.log("Этого не может быть");
+	} else if(time > 8 && time < 20) {
+			console.log('Время работать');
+		} else if(time < 24) {
+			console.log('Слишком поздно');
+			} else {
+				console.log('В сутках 24 часа!');
+				}
+}
+// workTime(18);
 
-alert(mainList.budget / 30);
+function calcBudget(price) {
+	alert("Ежедневный бюджет: " + (mainList.budget / 30));
+}
+// calcBudget();
+
+function discontSystem(price) {
+	if (mainList.discount == true) {
+		console.log(price = price / 100 * 80);
+	}
+}
+// discontSystem(1000);
+
+function chooseEmployers() {
+	for (let i = 0; i < 4; i++) {
+		let a = prompt("Имя сотрудника № " + (i + 1));
+		if ((typeof(a)) === 'string' && (typeof(a)) != undefined && a != '' && a.length < 50 ) {
+			console.log('Все верно');
+			mainList.employers[i] = a;
+		} else {
+			alert("Вы ввели неправильное значение или вообще ничего не ввели, попробуйте еще раз");
+			i = i - 1;
+		}
+	}
+}
+chooseEmployers();
 
 console.log(mainList);
